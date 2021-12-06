8fcfaff8c58cf106e556029eb938c33faeefcf26
andiwand <stefl.andreas@gmail.com>
2021-12-06 11:35:40 +0100

    tmp

--- raspi/myusbgadget.sh
+++ raspi/myusbgadget.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
 # references
-#  - https://irq5.io/2016/12/22/raspberry-pi-zero-as-multiple-usb-gadgets/
+# - https://irq5.io/2016/12/22/raspberry-pi-zero-as-multiple-usb-gadgets/
+# - audio
 #  - https://raspberrypi.stackexchange.com/questions/120496/bit-perfect-usb-audio-gadget
 #  - https://stackoverflow.com/questions/65959092/usb-audio-gadget-driver-how-to-rename-define-displayed-uac1-device-name-at-host
+#  - https://www.reddit.com/r/raspberry_pi/comments/lcnq6v/need_help_struggling_to_set_up_a_composite/
+#  - https://www.audiosciencereview.com/forum/index.php?threads/raspberry-pi-as-usb-to-i2s-adapter.8567/
 
 echo "insert usb gadget"
 
@@ -35,13 +38,14 @@ echo "irq5 labs"        > strings/0x407/manufacturer
 echo "Pi Zero Gadget"   > strings/0x407/product
 echo "MDB Config1"      > strings/0x407/configuration
 
-#echo 1 > os_desc/use
-#echo 0xcd > os_desc/b_vendor_code
-#echo MSFT100 > os_desc/qw_sign
+echo 1 > os_desc/use
+echo 0xcd > os_desc/b_vendor_code
+echo MSFT100 > os_desc/qw_sign
 
 mkdir -p configs/c.1
 echo 250 > configs/c.1/MaxPower
 #echo 0x80 > configs/c.1/bmAttributes
+ln -s configs/c.1 os_desc
 
 # mass storage
 mkdir -p functions/mass_storage.usb0
@@ -61,8 +65,9 @@ ln -s functions/uac1.usb0 configs/c.1
 
 # network
 mkdir -p functions/rndis.usb0
+echo "RNDIS" > functions/rndis.usb0/os_desc/interface.rndis/compatible_id # Windows RNDIS Drivers
+echo "5162001" > functions/rndis.usb0/os_desc/interface.rndis/sub_compatible_id #Windows RNDIS 6.0 Driver
 ln -s functions/rndis.usb0 configs/c.1
-ln -s configs/c.1 os_desc
 
 udevadm settle -t 5 || :
 ls /sys/class/udc > UDC
